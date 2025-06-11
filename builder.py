2324bbacbb639d3def93085f642a354fa0015f64
OussamaMahjour <MahjourOussama@gmail.com>
2025-06-11 20:31:17 +0100

    update

--- builder.py
+++ builder.py
@@ -26,40 +26,40 @@ if __name__ == "__main__":
     data_path = f"{data_root_path}/v{version}/data/"
     rfm_path=f"{data_root_path}/v{version}/rfm/"
     
-    os.system('cls||clear')
+    
     print("Loading Data ...")
     data = load_data()
     
-    os.system('cls||clear')
+    
     print("Processing Data ...")
     data_processed = data_processor.process(data)
     
-    os.system('cls||clear')
+    
     print("Saving Data ...")
     os.makedirs(data_path, exist_ok=True)
     joblib.dump(data_processor,data_path+"data_processor.pkl")
     data.to_csv(data_path+"data.csv",index=False)
     
-    os.system('cls||clear')
+    
     print("Converting Data to RFM ...")
     rfm = data_to_rfm(data=data_processed)
     
-    os.system('cls||clear')
+    
     print("Processing RFM ...")
     rfm_processed = rfm_processor.process(rfm)
     
-    os.system('cls||clear')
+    
     print("Saving RFM ...")
     os.makedirs(rfm_path, exist_ok=True)
     joblib.dump(rfm_processor,rfm_path+"rfm_processor.pkl")
     rfm.to_csv(rfm_path+"rfm.csv",index=False)
     
-    os.system('cls||clear')
+    
     print("Training the model ...")
     model.fit(rfm_processed.to_numpy())
     
     
-    os.system('cls||clear')
+    
     print("Saving Model ...")
     
     
