9e3edff36d9b4d9a39b31e23fe1e7eccafe61c02
OussamaMahjour <MahjourOussama@gmail.com>
2025-06-18 13:09:21 +0100

    update

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ models_root_path="/models"
 data_root_path="/data"
 
 
-model_server = "http://13.48.5.118"
-data_server="http://16.170.221.129"
+model_server = "http://13.53.36.159"
+data_server="http://51.21.219.250"
 
 
 
